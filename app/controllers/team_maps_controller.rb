d45f0b3a0967404efdab8335f50d445f8f1b3360
leanh173 <leanh173@gmail.com>
2018-06-05 15:37:25 +0700

    fix go to map by code

--- app/controllers/team_maps_controller.rb
+++ app/controllers/team_maps_controller.rb
@@ -6,7 +6,7 @@ class TeamMapsController < ApplicationController
   def create
     if params[:go_to_map]
       @team_map = TeamMap.find_by(code: params[:team_map][:code])
-      redirect_to team_map_path(code: @team_map.code) if @team_map
+      return redirect_to new_member_location_path(code: @team_map.code) if @team_map
       @team_map.errors.add(:code, I18n.t("errors.invalid_team_map_code"))
       render :new
     else
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :member_locations, only: [:create]
 
   get "team_maps/:code/:member_name" => 'member_locations#show', as: :member_location
-  get "team_maps/:code/member/new" => 'member_locations#new', as: :new_member_location
+  get "team_maps/:code/members/new" => 'member_locations#new', as: :new_member_location
 
   root "team_maps#new"
   get 'grayscales/index'
